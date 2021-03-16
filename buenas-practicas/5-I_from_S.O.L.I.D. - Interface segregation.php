fa1716cb4f1ef4f93d3abc9f9a1b3bc5f3abd75a
jotajotaramos <jramosj@gmail.com>
2021-03-16 08:47:13 +0100

    I from SOLID

---
+++ buenas-practicas/5-I_from_S.O.L.I.D. - Interface segregation.php
@@ -0,0 +1,104 @@
+### Ejemplo de código de aplicación del principio de segregación del interfaz. I de S.O.L.I.D.
+
+<?php
+
+/**
+ * Artículo de referencia
+ *
+ *
+ *
+ */
+
+/**
+ * Violación del principio
+ *
+ */
+
+interface Animal{
+
+	//método que calcula la velocidad en tierra
+	public function calculateEarthSpeed(){}
+
+	//método que calcule la velocidad en el aire
+	public function calculateAirSpeed(){}
+	
+}
+
+//clase vaca
+class Cow implements Animal{
+
+	//método que calcula la velocidad en tierra
+	public function calculateEarthSpeed(){
+	
+		//funcionalidad de cálculo de velocidad en tierra
+	
+	}
+
+	//método que calcule la velocidad en el aire
+	public function calculateAirSpeed(){
+	
+		//funcionalidad de cálculo de velocidad en aire
+	
+	}
+
+}
+
+//clase pato
+class Duck implements Animal{
+
+	//método que calcula la velocidad en tierra
+	public function calculateEarthSpeed(){
+	
+		//funcionalidad de cálculo de velocidad en tierra
+	
+	}
+
+	//método que calcule la velocidad en el aire
+	public function calculateAirSpeed(){
+	
+		//funcionalidad de cálculo de velocidad en aire
+	
+	}
+
+}
+
+/**
+ * Principio corregido
+ *
+ */
+
+interface Animal{
+
+	//método que calcula la velocidad
+	public function calculateSpeed(){}
+	
+}
+
+//clase vaca
+class Cow implements Animal{
+    
+    const EARTH_FACTOR= xxx;
+
+	//método que calcula la velocidad en tierra
+	public function calculateSpeed(){
+	
+		//funcionalidad de cálculo de velocidad en tierra y que utiliza el factor EARTH_FACTOR
+	
+	}
+
+}
+
+//clase pato
+class Duck implements Animal{
+    
+    const AIR_FACTOR= xxx;
+
+	//método que calcula la velocidad en tierra
+	public function calculateSpeed(){
+	
+		//funcionalidad de cálculo de velocidad en tierra y que utiliza el factor AIR_FACTOR
+	
+	}
+
+
+}
